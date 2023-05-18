5b2cac718589e73fce57b1bb7af6df03c80b7b83
xaleoryx <99973225+xaleoryx@users.noreply.github.com>
2023-05-18 15:38:19 +0300

    update

--- config.py
+++ config.py
@@ -11,78 +11,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import random## undefined
+import random## Import the random module for generating random numbers.
 
-import numpy as np## undefined
-import torch## undefined
-from torch.backends import cudnn## undefined
+import numpy as np## Import the numpy module for numerical computations.
+import torch## Import the torch module for machine learning.
+from torch.backends import cudnn## Import cudnn for GPU acceleration.
 
 # Random seed to maintain reproducible results
-random.seed(0)## undefined
-torch.manual_seed(0)## undefined
-np.random.seed(0)## undefined
+random.seed(0)## Set random seed for reproducibility.
+torch.manual_seed(0)## Set PyTorch random seed for reproducibility.
+np.random.seed(0)## Set numpy random seed for reproducibility.
 # Use GPU for training by default
-device = torch.device("cuda", 0)## undefined
+device = torch.device("cuda", 0)## Set device to use GPU for training.
 # Turning on when the image size does not change during training can speed up training
-cudnn.benchmark = True## undefined
+cudnn.benchmark = True## Enable cuDNN benchmark mode for faster training.
 # Model arch name
-model_arch_name = "googlenet"## undefined
+model_arch_name = "googlenet"## Set the model architecture name.
 # Model number class
-model_num_classes = 1000## undefined
+model_num_classes = 1000## Set the number of classes in the model.
 # Current configuration parameter method
 mode = "train"
 # Experiment name, easy to save weights and log files
-exp_name = f"{model_arch_name.upper()}-ImageNet_1K"## undefined
+exp_name = f"{model_arch_name.upper()}-ImageNet_1K"## Set the experiment name for saving weights and logs.
 
 if mode == "train":
     # Dataset address
-    train_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_train"## undefined
-    valid_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val"## undefined
+    train_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_train"## Directory containing training images.
+    valid_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val"## Directory containing validation images.
 
-    image_size = 224## undefined
-    batch_size = 128## undefined
-    num_workers = 4## undefined
+
+    image_size = 224## Directory containing validation images.
+    batch_size = 128## Number of samples per batch during training.
+    num_workers = 4## Number of worker threads for loading data.
 
     # The address to load the pretrained model
-    pretrained_model_weights_path = "./results/pretrained_models/GoogleNet-ImageNet_1K-32d70693.pth.tar"## undefined
+    pretrained_model_weights_path = "./results/pretrained_models/GoogleNet-ImageNet_1K-32d70693.pth.tar"## Path to load the pretrained model weights.
 
     # Incremental training and migration training
     resume = ""
 
     # Total num epochs
-    epochs = 600## undefined
+    epochs = 600## Total number of epochs to train the model.
 
     # Loss parameters
-    loss_label_smoothing = 0.1## undefined
-    loss_aux3_weights = 1.0## undefined
-    loss_aux2_weights = 0.3## undefined
-    loss_aux1_weights = 0.3## undefined
+    loss_label_smoothing = 0.1## Label smoothing parameter for the loss function.
+    loss_aux3_weights = 1.0## Weight for auxiliary output 3 in the loss function.
+    loss_aux2_weights = 0.3## Weight for auxiliary output 2 in the loss function.
+    loss_aux1_weights = 0.3## Weight for auxiliary output 1 in the loss function.
 
     # Optimizer parameter
-    model_lr = 0.1## undefined
-    model_momentum = 0.9## undefined
-    model_weight_decay = 2e-05## undefined
-    model_ema_decay = 0.99998## undefined
+    model_lr = 0.1## Learning rate for the optimizer.
+    model_momentum = 0.9## Momentum parameter for the optimizer.
+    model_weight_decay = 2e-05## Weight decay (L2 penalty) for the optimizer.
+    model_ema_decay = 0.99998## Decay rate for exponential moving average of model parameters.
 
     # Learning rate scheduler parameter
-    lr_scheduler_T_0 = epochs // 4## undefined
-    lr_scheduler_T_mult = 1## undefined
-    lr_scheduler_eta_min = 5e-5## undefined
+    lr_scheduler_T_0 = epochs // 4## Number of epochs before dropping the learning rate.
+    lr_scheduler_T_mult = 1## Multiplicative factor for dropping the learning rate.
+    lr_scheduler_eta_min = 5e-5## Minimum learning rate for the scheduler.
 
     # How many iterations to print the training/validate result
-    train_print_frequency = 200## undefined
-    valid_print_frequency = 20## undefined
+    train_print_frequency = 200## Number of iterations to print training results.
+    valid_print_frequency = 20## Number of iterations to print validate reesults.
 
 if mode == "test":
     # Test data address
-    test_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val"## undefined
+    test_image_dir = "./data/ImageNet_1K/ILSVRC2012_img_val"## Directory for the test dataset.
 
     # Test dataloader parameters
-    image_size = 224## undefined
-    batch_size = 256## undefined
-    num_workers = 4## undefined
+    image_size = 224## The size of the input images for testing.
+    batch_size = 256## The batch size for testing.
+    num_workers = 4## The number of worker threads for data loading during testing.
 
     # How many iterations to print the testing result
-    test_print_frequency = 20## undefined
+    test_print_frequency = 20## The frequency to print the testing result during testing.
 
     model_weights_path = "./results/pretrained_models/GoogleNet-ImageNet_1K-32d70693.pth.tar"
--- dataset.py
+++ dataset.py
@@ -11,20 +11,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import queue## undefined
-import sys## undefined
-import threading## undefined
-from glob import glob## undefined
+import queue## Import the queue module for multi-threading support.
+import sys## Import the sys module for system-specific parameters and functions.
+import threading## Import the threading module for multi-threading support.
+from glob import glob## Import the glob function for file pattern matching.
 
-import cv2## undefined
-import torch## undefined
-from PIL import Image## undefined
-from torch.utils.data import Dataset, DataLoader## undefined
-from torchvision import transforms## undefined
-from torchvision.datasets.folder import find_classes## undefined
-from torchvision.transforms import TrivialAugmentWide## undefined
+import cv2## Import the cv2 module for computer vision functions using OpenCV.
+import torch## Import the torch module for machine learning.
+from PIL import Image## Import the Image module from PIL (Python Imaging Library).
+from torch.utils.data import Dataset, DataLoader## Import Dataset and DataLoader from torch.utils.data module for creating custom datasets and data loaders.
+from torchvision import transforms##  Import the transforms module from torchvision for image transformations.
+from torchvision.datasets.folder import find_classes## Import the find_classes function from torchvision.datasets.folder module to find the classes in a dataset directory.
+from torchvision.transforms import TrivialAugmentWide## Import the TrivialAugmentWide transformation from torchvision.transforms module for data augmentation.
 
-import imgproc## undefined
+import imgproc## Import the imgproc module (assuming it's a custom module).
 
 __all__ = [
     "ImageDataset",
@@ -32,16 +32,17 @@ __all__ = [
 ]
 
 # Image formats supported by the image processing library
-IMG_EXTENSIONS = ("jpg", "jpeg", "png", "ppm", "bmp", "pgm", "tif", "tiff", "webp")## undefined
+IMG_EXTENSIONS = ("jpg", "jpeg", "png", "ppm", "bmp", "pgm", "tif", "tiff", "webp")## defines a tuple IMG_EXTENSIONS that contains a list of image file extensions.
 
 # The delimiter is not the same between different platforms
-if sys.platform == "win32":## undefined
-    delimiter = "\\"## undefined
+if sys.platform == "win32":## checks if the current platform is Windows.
+    delimiter = "\\"## Set the delimiter to backslash "\\" if the platform is Windows.
 else:
-    delimiter = "/"## undefined
+    delimiter = "/"## Set the delimiter to forward slash "/" for non-Windows platforms.
 
 
-class ImageDataset(Dataset):## undefined
+
+class ImageDataset(Dataset):## defines a class named ImageDataset that inherits from the Dataset class.
     """Define training/valid dataset loading methods.
 
     Args:
@@ -52,71 +53,72 @@ class ImageDataset(Dataset):## undefined
     """
 
     def __init__(self, image_dir: str, image_size: int, mode: str) -> None:## undefined
-        super(ImageDataset, self).__init__()## undefined
+        super(ImageDataset, self).__init__()## Initialize the parent class (Dataset).
         # Iterate over all image paths
-        self.image_file_paths = glob(f"{image_dir}/*/*")## undefined
+        self.image_file_paths = glob(f"{image_dir}/*/*")## Get the file paths of all images in the image directory.
         # Form image class label pairs by the folder where the image is located
-        _, self.class_to_idx = find_classes(image_dir)## undefined
-        self.image_size = image_size## undefined
-        self.mode = mode## undefined
-        self.delimiter = delimiter## undefined
+        _, self.class_to_idx = find_classes(image_dir)## Find the classes in the image directory and get the class-to-index mapping.
+        self.image_size = image_size## Set the image size for resizing images.
+        self.mode = mode## Set the mode of the dataset (training or validation).
+        self.delimiter = delimiter## Set the delimiter for file paths based on the platform.
 
-        if self.mode == "Train":## undefined
+        if self.mode == "Train":## checks if the mode is "Train".
             # Use PyTorch's own data enhancement to enlarge and enhance data
-            self.pre_transform = transforms.Compose([## undefined
-                transforms.RandomResizedCrop(self.image_size),## undefined
-                TrivialAugmentWide(),## undefined
-                transforms.RandomRotation([0, 270]),## undefined
-                transforms.RandomHorizontalFlip(0.5),## undefined
-                transforms.RandomVerticalFlip(0.5),## undefined
+            self.pre_transform = transforms.Compose([## initializes a transformation pipeline for pre-processing the image data.
+                transforms.RandomResizedCrop(self.image_size),## Randomly crop and resize the image
+                TrivialAugmentWide(),## Apply a wide range of simple data augmentations
+                transforms.RandomRotation([0, 270]),##  Randomly rotate the image
+                transforms.RandomHorizontalFlip(0.5),## Randomly flip the image horizontally
+                transforms.RandomVerticalFlip(0.5),## Randomly flip the image vertically
             ])
-        elif self.mode == "Valid" or self.mode == "Test":## undefined
+        elif self.mode == "Valid" or self.mode == "Test":## checks if the mode is either "Valid" or "Test".
             # Use PyTorch's own data enhancement to enlarge and enhance data
-            self.pre_transform = transforms.Compose([## undefined
-                transforms.Resize(256),## undefined
-                transforms.CenterCrop([self.image_size, self.image_size]),## undefined
+            self.pre_transform = transforms.Compose([## initializes a transformation pipeline for pre-processing the image data.
+                transforms.Resize(256),## Resize the image to 256x256
+                transforms.CenterCrop([self.image_size, self.image_size]),## Crop the image to the specified size
             ])
         else:
-            raise "Unsupported data read type. Please use `Train` or `Valid` or `Test`"## undefined
+            raise "Unsupported data read type. Please use `Train` or `Valid` or `Test`"## raises an exception with an error message when an unsupported data read type is encountered.
 
-        self.post_transform = transforms.Compose([## undefined
-            transforms.ConvertImageDtype(torch.float),## undefined
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])## undefined
+        self.post_transform = transforms.Compose([## initializes a transformation pipeline for post-processing the image data.
+            transforms.ConvertImageDtype(torch.float),## Convert the image to float type
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])## Normalize the image with mean and standard deviation
+])
         ])
 
-    def __getitem__(self, batch_index: int) -> [torch.Tensor, int]:## undefined
+    def __getitem__(self, batch_index: int) -> [torch.Tensor, int]:## defines the __getitem__ method of the ImageDataset class, which is responsible for returning an item (image and target) from the dataset given a batch index.
         image_dir, image_name = self.image_file_paths[batch_index].split(self.delimiter)[-2:]## undefined
         # Read a batch of image data
-        if image_name.split(".")[-1].lower() in IMG_EXTENSIONS:## undefined
-            image = cv2.imread(self.image_file_paths[batch_index])## undefined
-            target = self.class_to_idx[image_dir]## undefined
+        if image_name.split(".")[-1].lower() in IMG_EXTENSIONS:## checks if the file extension of the image (extracted from image_name) is present in the IMG_EXTENSIONS tuple, indicating that it is a supported image format.
+            image = cv2.imread(self.image_file_paths[batch_index])## Read the image using OpenCV
+            target = self.class_to_idx[image_dir]##  Get the class index for the image
         else:
             raise ValueError(f"Unsupported image extensions, Only support `{IMG_EXTENSIONS}`, "
                              "please check the image file extensions.")
 
         # BGR to RGB
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)## undefined
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)## Convert image from BGR to RGB
 
         # OpenCV convert PIL
-        image = Image.fromarray(image)## undefined
+        image = Image.fromarray(image)## Convert image to PIL format
 
         # Data preprocess
-        image = self.pre_transform(image)## undefined
+        image = self.pre_transform(image)## Apply pre-processing transformations
 
         # Convert image data into Tensor stream format (PyTorch).
         # Note: The range of input and output is between [0, 1]
-        tensor = imgproc.image_to_tensor(image, False, False)## undefined
+        tensor = imgproc.image_to_tensor(image, False, False)## Convert image to Tensor format
 
         # Data postprocess
-        tensor = self.post_transform(tensor)## undefined
+        tensor = self.post_transform(tensor)## applies post-processing transformations to the image tensor.
 
-        return {"image": tensor, "target": target}## undefined
+        return {"image": tensor, "target": target}## returns a dictionary containing the image tensor as the value for the key "image" and the target (class index) as the value for the key "target".
 
     def __len__(self) -> int:
-        return len(self.image_file_paths)## undefined
+        return len(self.image_file_paths)## returns the length of the image_file_paths list, which represents the total number of images in the dataset.
 
 
-class PrefetchGenerator(threading.Thread):## undefined
+class PrefetchGenerator(threading.Thread):## defines a class named PrefetchGenerator that extends the threading.Thread class. 
     """A fast data prefetch generator.
 
     Args:
@@ -124,23 +126,23 @@ class PrefetchGenerator(threading.Thread):## undefined
         num_data_prefetch_queue (int): How many early data load queues.
     """
 
-    def __init__(self, generator, num_data_prefetch_queue: int) -> None:## undefined
-        threading.Thread.__init__(self)## undefined
-        self.queue = queue.Queue(num_data_prefetch_queue)## undefined
-        self.generator = generator## undefined
+    def __init__(self, generator, num_data_prefetch_queue: int) -> None:## Initialize the prefetch generator thread by setting up the data queue and storing the data generator object. 
+        threading.Thread.__init__(self)## Initialize the parent class (Thread)
+        self.queue = queue.Queue(num_data_prefetch_queue)## Create a queue for data prefetching
+        self.generator = generator## Store the generator object
         self.daemon = True
-        self.start()## undefined
+        self.start()## Start the thread execution
 
-    def run(self) -> None:## undefined
-        for item in self.generator:## undefined
-            self.queue.put(item)## undefined
-        self.queue.put(None)## undefined
+    def run(self) -> None:## Execute the thread's main logic.
+        for item in self.generator:## iterates over the data generator
+            self.queue.put(item)## Put the item into the queue for prefetching
+        self.queue.put(None)## Put None into the queue to indicate the end of data generation
 
     def __next__(self):
-        next_item = self.queue.get()## undefined
-        if next_item is None:## undefined
-            raise StopIteration## undefined
-        return next_item## undefined
+        next_item = self.queue.get()## Get the next item from the queue
+        if next_item is None:## checks if the next_item variable is None.
+            raise StopIteration## Raise StopIteration when there are no more items
+        return next_item## returns the next_item from the prefetch queue. 
 
     def __iter__(self):
         return self
